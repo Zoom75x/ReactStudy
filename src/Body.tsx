f5f573e86796adfe975712a8fae1a1b2892b095b
ViVarkina <vikavarkina@gmail.com>
2024-04-21 12:39:40 +0300

    21.04

---
+++ src/Body.tsx
@@ -0,0 +1,11 @@
+export const Body=()=>{
+    const name = "Иван"
+    const hello = "Привет"
+    return (
+        <div className={"body"}>
+            <>{name}</>
+            <div>{hello}</div>
+        </div>
+    )
+
+}
